79595d405db703b7c1946e29bf2cfcfd32037d4c
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-09-07 22:41:05 +0700

    Add contact store book
    Add contact store book

--- Contact-Store-scripts/Book_contact.cs
+++ Contact-Store-scripts/Book_contact.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Carrot;
+using System.Collections;
 using UnityEngine;
 
 public class Book_contact : MonoBehaviour
@@ -18,7 +19,13 @@ public class Book_contact : MonoBehaviour
 	public Sprite sp_icon_add_phone;
 	public Sprite sp_icon_update_phone;
 
-	private int index_del = -1;
+	[Header("Contact Book Field")]
+	private Carrot.Carrot_Box_Item item_field_name;
+    private Carrot.Carrot_Box_Item item_field_phone;
+    private Carrot.Carrot_Box_Item item_field_email;
+    private Carrot.Carrot_Box_Item item_field_sex;
+
+    private int index_del = -1;
 	private Carrot.Carrot_Window_Msg msg;
 	private Carrot.Carrot_Box box;
 
@@ -137,35 +144,62 @@ public class Book_contact : MonoBehaviour
 		box.set_icon(this.sp_icon_add_phone);
 		box.set_title("Add New Contact Book");
 
-        Carrot.Carrot_Box_Item item_name=box.create_item("item_name");
-		item_name.set_icon(this.app.carrot.user.icon_user_info);
-		item_name.set_title("Full name");
-		item_name.set_tip("Enter the contact's full name");
-		item_name.set_type(Carrot.Box_Item_Type.box_value_input);
-		item_name.check_type();
+        this.item_field_name=box.create_item("item_name");
+        this.item_field_name.set_icon(this.app.carrot.user.icon_user_info);
+        this.item_field_name.set_title("Full name");
+        this.item_field_name.set_tip("Enter the contact's full name");
+        this.item_field_name.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_name.check_type();
 
-		Carrot.Carrot_Box_Item item_phone = box.create_item("item_phone");
-		item_phone.set_icon(this.app.carrot.user.icon_user_info);
-		item_phone.set_title("Phone");
-		item_phone.set_tip("Enter Phone Number");
-		item_phone.set_type(Carrot.Box_Item_Type.box_value_input);
-		item_phone.check_type();
+		this.item_field_phone = box.create_item("item_phone");
+        this.item_field_phone.set_icon(this.app.carrot.user.icon_user_info);
+        this.item_field_phone.set_title("Phone");
+        this.item_field_phone.set_tip("Enter Phone Number");
+        this.item_field_phone.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_phone.check_type();
 
-		Carrot.Carrot_Box_Btn_Panel panel=box.create_panel_btn();
+        this.item_field_email = box.create_item("item_email");
+        this.item_field_email.set_icon(this.app.carrot.icon_carrot_mail);
+        this.item_field_email.set_title("Email");
+        this.item_field_email.set_tip("Enter Email Addreess");
+        this.item_field_email.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_email.check_type();
+
+        this.item_field_sex = box.create_item("item_sex");
+        this.item_field_sex.set_icon(this.app.carrot.icon_carrot_mail);
+        this.item_field_sex.set_title("Sex");
+        this.item_field_sex.set_tip("Select Your Sex");
+        this.item_field_sex.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_sex.check_type();
+
+        Carrot.Carrot_Box_Btn_Panel panel=box.create_panel_btn();
 		Carrot.Carrot_Button_Item btn_done=panel.create_btn("btn_done");
+		btn_done.set_label("Done");
 		btn_done.set_icon_white(this.app.carrot.icon_carrot_done);
 		btn_done.set_bk_color(this.app.carrot.color_highlight);
 		btn_done.set_act_click(() => this.create_contact_done());
 
 		Carrot.Carrot_Button_Item btn_cancel = panel.create_btn("btn_cancel");
-		btn_cancel.set_icon_white(this.app.carrot.icon_carrot_cancel);
+        btn_cancel.set_label("Cancel");
+        btn_cancel.set_icon_white(this.app.carrot.icon_carrot_cancel);
 		btn_cancel.set_bk_color(this.app.carrot.color_highlight);
 		btn_cancel.set_act_click(()=>box.close());
 	}
 
 	private void create_contact_done()
     {
+		IDictionary contact_data = (IDictionary) Json.Deserialize("{}");
+        contact_data["name"]= this.item_field_name.get_val();
+        contact_data["phone"] = this.item_field_phone.get_val();
+        contact_data["email"] = this.item_field_email.get_val();
+        contact_data["sex"] = this.item_field_sex.get_val();
+        contact_data["type"] = "phonebook";
+        contact_data["index"] = this.length;
+
+        this.create(contact_data);
 		this.box.close();
+
+        this.app.carrot.show_msg("Contact Store", "Contact Archive Successful!", Carrot.Msg_Icon.Success);
     }
 
 }
