aca53114b3d8e6b037f8b7223efc18abcc618f9f
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-09-13 21:30:05 +0700

    Update book phone add
    Update book phone add

--- Contact-Store-scripts/Book_contact.cs
+++ Contact-Store-scripts/Book_contact.cs
@@ -1,6 +1,7 @@
 ﻿using Carrot;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Book_contact : MonoBehaviour
 {
@@ -24,6 +25,7 @@ public class Book_contact : MonoBehaviour
     private Carrot.Carrot_Box_Item item_field_phone;
     private Carrot.Carrot_Box_Item item_field_email;
     private Carrot.Carrot_Box_Item item_field_sex;
+    private Carrot.Carrot_Box_Item item_field_address;
 
     private int index_del = -1;
 	private Carrot.Carrot_Window_Msg msg;
@@ -145,18 +147,22 @@ public class Book_contact : MonoBehaviour
 		box.set_title("Add New Contact Book");
 
         this.item_field_name=box.create_item("item_name");
-        this.item_field_name.set_icon(this.app.carrot.user.icon_user_info);
+        this.item_field_name.set_icon(this.app.carrot.user.icon_user_login_true);
         this.item_field_name.set_title("Full name");
         this.item_field_name.set_tip("Enter the contact's full name");
         this.item_field_name.set_type(Carrot.Box_Item_Type.box_value_input);
         this.item_field_name.check_type();
+		this.item_field_name.set_key_lang_title("user_name");
+		this.item_field_name.load_lang_data();
 
 		this.item_field_phone = box.create_item("item_phone");
-        this.item_field_phone.set_icon(this.app.carrot.user.icon_user_info);
+        this.item_field_phone.set_icon(this.app.carrot.icon_carrot_phone);
         this.item_field_phone.set_title("Phone");
         this.item_field_phone.set_tip("Enter Phone Number");
         this.item_field_phone.set_type(Carrot.Box_Item_Type.box_value_input);
         this.item_field_phone.check_type();
+        this.item_field_phone.set_key_lang_title("user_phone");
+        this.item_field_phone.load_lang_data();
 
         this.item_field_email = box.create_item("item_email");
         this.item_field_email.set_icon(this.app.carrot.icon_carrot_mail);
@@ -164,23 +170,40 @@ public class Book_contact : MonoBehaviour
         this.item_field_email.set_tip("Enter Email Addreess");
         this.item_field_email.set_type(Carrot.Box_Item_Type.box_value_input);
         this.item_field_email.check_type();
+        this.item_field_email.set_key_lang_title("user_email");
+        this.item_field_email.load_lang_data();
 
         this.item_field_sex = box.create_item("item_sex");
-        this.item_field_sex.set_icon(this.app.carrot.icon_carrot_mail);
+        this.item_field_sex.set_icon(this.app.carrot.icon_carrot_sex);
         this.item_field_sex.set_title("Sex");
         this.item_field_sex.set_tip("Select Your Sex");
-        this.item_field_sex.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_sex.set_type(Carrot.Box_Item_Type.box_value_dropdown);
         this.item_field_sex.check_type();
+        this.item_field_sex.set_key_lang_title("user_sex");
+        this.item_field_sex.load_lang_data();
+
+		this.item_field_sex.dropdown_val.ClearOptions();
+		this.item_field_sex.dropdown_val.options.Add(new Dropdown.OptionData(){text=PlayerPrefs.GetString("user_sex_boy", "Male") });
+        this.item_field_sex.dropdown_val.options.Add(new Dropdown.OptionData(){text=PlayerPrefs.GetString("user_sex_girl", "Female") });
+
+        this.item_field_address = box.create_item("item_address");
+        this.item_field_address.set_icon(this.app.carrot.icon_carrot_address);
+        this.item_field_address.set_title("Address");
+        this.item_field_address.set_tip("Enter the contact's permanent address");
+        this.item_field_address.set_type(Carrot.Box_Item_Type.box_value_input);
+        this.item_field_address.check_type();
+        this.item_field_address.set_key_lang_title("user_address");
+        this.item_field_address.load_lang_data();
 
         Carrot.Carrot_Box_Btn_Panel panel=box.create_panel_btn();
 		Carrot.Carrot_Button_Item btn_done=panel.create_btn("btn_done");
-		btn_done.set_label("Done");
+		btn_done.set_label(PlayerPrefs.GetString("done","Done"));
 		btn_done.set_icon_white(this.app.carrot.icon_carrot_done);
 		btn_done.set_bk_color(this.app.carrot.color_highlight);
 		btn_done.set_act_click(() => this.create_contact_done());
 
 		Carrot.Carrot_Button_Item btn_cancel = panel.create_btn("btn_cancel");
-        btn_cancel.set_label("Cancel");
+        btn_cancel.set_label(PlayerPrefs.GetString("cancel","Cancel"));
         btn_cancel.set_icon_white(this.app.carrot.icon_carrot_cancel);
 		btn_cancel.set_bk_color(this.app.carrot.color_highlight);
 		btn_cancel.set_act_click(()=>box.close());
@@ -193,8 +216,9 @@ public class Book_contact : MonoBehaviour
         contact_data["phone"] = this.item_field_phone.get_val();
         contact_data["email"] = this.item_field_email.get_val();
         contact_data["sex"] = this.item_field_sex.get_val();
-        contact_data["type"] = "phonebook";
+        contact_data["type_item"] = "phonebook";
         contact_data["index"] = this.length;
+		contact_data["user_id"] = "conact"+this.app.carrot.generateID();
 
         this.create(contact_data);
 		this.box.close();
